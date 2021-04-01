460bdf27f42b63d34f6eca82dbbbba81e1f1555b
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-01 18:40:20 +0200

    Initial commit

---
+++ backend/src/main/java/com/wat/pz/costshare/CostShareApplication.java
@@ -0,0 +1,13 @@
+package com.wat.pz.costshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostShareApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/wat/pz/costshare/CostShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wat.pz.costshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
