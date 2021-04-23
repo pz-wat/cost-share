12a5712be2e72f242cf450dd44388685d2272133
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-23 18:03:29 +0200

    Create UserDetails and UserDetailsService implementation

---
+++ backend/src/main/java/com/wat/pz/costshare/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.wat.pz.costshare.security.services;
+
+import com.wat.pz.costshare.entity.User;
+import com.wat.pz.costshare.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
