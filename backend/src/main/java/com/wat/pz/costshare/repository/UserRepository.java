a2b3affd0d016db597a386ce43717c4c7b43c8f3
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-23 17:56:15 +0200

    Create RoleRepository and update UserRepository

--- backend/src/main/java/com/wat/pz/costshare/repository/UserRepository.java
+++ backend/src/main/java/com/wat/pz/costshare/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.wat.pz.costshare.repository;
 import com.wat.pz.costshare.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
