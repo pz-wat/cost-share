f42d171f36c7029468831c85110655ac350c42f0
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-23 22:07:04 +0200

    Add cors

--- backend/src/main/java/com/wat/pz/costshare/controller/AuthController.java
+++ backend/src/main/java/com/wat/pz/costshare/controller/AuthController.java
@@ -5,12 +5,10 @@ import com.wat.pz.costshare.dto.request.SignupRequest;
 import com.wat.pz.costshare.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/auth")
 public class AuthController {
 
