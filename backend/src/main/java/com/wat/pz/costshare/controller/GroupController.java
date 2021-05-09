8a8b7dffe53528fa3b1e6719d0092e7e31f3d97b
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-05-09 21:00:30 +0200

    Add cross origin to group controller

--- backend/src/main/java/com/wat/pz/costshare/controller/GroupController.java
+++ backend/src/main/java/com/wat/pz/costshare/controller/GroupController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class GroupController {
 
     private final GroupService groupService;
